520cc0d9d391858412efa5b80e500907f7e2e7ee
zachcraig92 <zachcraig19@gmail.com>
2013-11-21 19:09:32 -0600

    Update gamepage.php

--- dev/gamepage.php
+++ dev/gamepage.php
@@ -1,4 +1,55 @@
-<?php include("inc/header.php"); ?>
-<?php include("inc/footer.php"); ?>
+
+<!DOCTYPE html>
+<html>
+    <?php include("inc/header.php"); ?>
+    
+        <!-- Javascript Dice -->
+    <div id="javadice">
+        <p>Don't have 5 dice?  Use these (Click on Dice to keep them each roll):</p>
+        <form name="dice">
+        <input type="hidden" id="dice1h" value="" />
+        <input type="hidden" id="dice1val" value="" />
+        <input type="hidden" id="dice2h" value="" />
+        <input type="hidden" id="dice2val" value="" />
+        <input type="hidden" id="dice3h" value="" />
+        <input type="hidden" id="dice3val" value="" />
+        <input type="hidden" id="dice4h" value="" />
+        <input type="hidden" id="dice4val" value="" />
+        <input type="hidden" id="dice5h" value="" />
+        <input type="hidden" id="dice5val" value="" />
+
+    <table> 
+     <tbody>
+       <tr> 
+	<th colspan="4">Total of Dice:
+        <td id="dicetotal"></td>
+       </tr>
+            
+    <tr>
+        <td id="dice1"></td>
+     
+        <td id="dice2"></td>
+       
+        <td id="dice3"></td>
+        
+        <td id="dice4"></td> 
+       
+        <td id="dice5"></td>
+        
+    </tr>
+    </tbody>
+
+    </table>
+        <input type="button" onclick="rollDice();" name="Roll" value="Roll"/>
+        <input type="button" onclick="resetDice();" name="Reset" value="Reset" />
+        
+        <div id="rolltotal"></div>
+    
+    </form>
+    </div>
+ <!-- End Javascript Dice -->
+  
+         <?php include("inc/footer.php"); ?>
+
     </body>
 </html>
