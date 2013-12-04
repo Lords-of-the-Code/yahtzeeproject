44ff8c8920a7cba5bc8733a0d5610ccac877aae5
zachcraig92 <zachcraig19@gmail.com>
2013-12-03 19:53:12 -0600

    Update userpage.php

--- dev/userpage.php
+++ dev/userpage.php
@@ -1,12 +1,13 @@
 <?php include("inc/header.php"); ?>
+<!--http://localhost/yahtzeeproject/dev/userpage.php-->
 
-<div id="Userpage">
+<div id="userpage">
       <h1>Welcome User Name</h1>
 
       <ul>
-        <li><a href="gamepage.php">New game</a></li>
-        <li><a href="">Stats</a></li>
-        <li><a href="">Friends</a></li>
+         <li><a href="gamepage.php">New game</a></li>
+        <li><a href="userstats.php">My Stats</a></li>
+        <li><a href="userfriends.php">My Friends</a></li>
       </ul>    
   
     </div>
