82f438e8d600752f5e647a8e9a02338d5a25eba9
zachcraig92 <zachcraig19@gmail.com>
2013-12-05 18:48:51 -0600

    Update game.js

--- dev/js/game.js
+++ dev/js/game.js
@@ -1,10 +1,215 @@
+// yahtzeeSS.js
+
 var totalGames;
-var rollCount = 0;
-        
+
 function setNumGames(number){
 	totalGames = number;
 }
-        
+
+// newFunction
+function validateScore(number, score, input, playeri, gamei) {
+	if (score > number * 5){
+		alert('Score Must be Less Than ' + number*5);
+		input.value = '';
+		input.focus();
+	} else if ((score % number) != 0) {
+		alert('Score Must be a Multiple of ' + number);
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function getUpperScore(playeri, gamei){
+	var acesScore = document.getElementById('aces'+playeri+gamei).value;
+	var twosScore = document.getElementById('twos'+playeri+gamei).value;
+	var threesScore = document.getElementById('threes'+playeri+gamei).value;
+	var foursScore = document.getElementById('fours'+playeri+gamei).value;
+	var fivesScore = document.getElementById('fives'+playeri+gamei).value;
+	var sixesScore = document.getElementById('sixes'+playeri+gamei).value;
+	
+	return parseInt(acesScore*1+twosScore*1+threesScore*1+foursScore*1+fivesScore*1+sixesScore*1);
+}
+
+function getUpperScoreAndBonus(playeri, gamei){
+	var totalScoreDiv = document.getElementById('totalscore'+playeri+gamei);
+	var score = getUpperScore(playeri, gamei);
+	totalScoreDiv.innerHTML = score;
+		
+	var totalDiv = document.getElementById('total'+playeri+gamei);
+	var bonusDiv = document.getElementById('bonus'+playeri+gamei);
+	var totalUpperDiv = document.getElementById('totalupper'+playeri+gamei);
+	
+	var totalScore;
+	if (score >= 63) {
+		bonusDiv.innerHTML = 'BONUS';
+		totalScore = parseInt(score+35);
+		totalDiv.innerHTML = totalScore;
+	} else {
+		bonusDiv.innerHTML = '';
+		totalDiv.innerHTML = score;
+		totalScore = score;
+	}
+	totalUpperDiv.innerHTML = totalScore;
+	return totalScore;
+}
+
+function validate3kind(score, input, playeri, gamei){
+	if (score > 30){
+		alert('Invalid 3 of a kind score ');
+		input.value = '';
+		document.getElementById('threekind'+playeri+gamei).focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validate4kind(score, input, playeri, gamei){
+	if (score > 30){
+		alert('Invalid 4 of a kind score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validateFullhouse(score, input, playeri, gamei){
+	if (score > 28){
+		alert('Invalid Full House score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validateSmstraight(score, input, playeri, gamei){
+	if (score != 30 && score != 0){
+		alert('Invalid Small Straight score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validateLgstraight(score, input, playeri, gamei){
+	if (score != 40 && score != 0){
+		alert('Invalid Large Straight score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validateChance(score, input, playeri, gamei){
+	if (score > 30){
+		alert('Invalid Chance score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function validateYahtzee(score, input, playeri, gamei){
+	var yahtzeeBonusDiv = document.getElementById('yahtzeeBonus'+playeri+gamei);
+	//make sure there is no bonus
+	yahtzeeBonusDiv.value = '0';
+	
+	if (score != 50 && score != 0){
+		alert('Invalid Yahtzee score ');
+		input.value = '';
+		input.focus();
+	} else {
+		getGrandTotal(playeri, gamei)
+	}
+}
+
+function addYahtzeeBonus(playeri, gamei){
+	var yahtzeeBonusDiv = document.getElementById('yahtzeeBonus'+playeri+gamei);
+	
+	//check to see if 50 yahtzee is done yet
+	var yahtzeeScore = document.getElementById('yahtzee'+playeri+gamei).value;
+	if (yahtzeeScore != undefined && yahtzeeScore == 50){
+		//we're good to go add 100 points
+		bonusCurrVal = yahtzeeBonusDiv.value;
+		if (bonusCurrVal != undefined && bonusCurrVal != ''){
+			yahtzeeBonusDiv.value = bonusCurrVal*1 + 100;
+		} else {
+			yahtzeeBonusDiv.value = '100';
+		}
+		getGrandTotal(playeri, gamei);
+	} else {
+		alert('You must first enter the 50 point Yahtzee Score before getting the Yahtzee Bonus.');
+	}
+}
+
+function removeYahtzeeBonus(playeri, gamei){
+	var yahtzeeBonusDiv = document.getElementById('yahtzeeBonus'+playeri+gamei);
+	
+	//we're good to go remove 100 points
+	bonusCurrVal = yahtzeeBonusDiv.value;
+	if (bonusCurrVal != undefined && bonusCurrVal != '' && bonusCurrVal != 0){
+		yahtzeeBonusDiv.value = bonusCurrVal*1 - 100;
+	} else if (bonusCurrVal == undefined || bonusCurrVal == 0){
+		alert('Are you crazy?!? You can not have a negative yahtzee bonus. ');
+	} else {
+		yahtzeeBonusDiv.value = '0';
+	}
+	getGrandTotal(playeri, gamei);
+}
+
+function getLowerScore(playeri, gamei){
+	var ThreekindScore = document.getElementById('threekind'+playeri+gamei).value;
+	var FourkindScore = document.getElementById('fourkind'+playeri+gamei).value;
+	var fullhouseScore = document.getElementById('fullhouse'+playeri+gamei).value;
+	var smstraightScore = document.getElementById('smstraight'+playeri+gamei).value;
+	var lgstraightScore = document.getElementById('lgstraight'+playeri+gamei).value;
+	var yahtzeeScore = document.getElementById('yahtzee'+playeri+gamei).value;
+	var chanceScore = document.getElementById('chance'+playeri+gamei).value;
+	var yahtzeeBonusScore = document.getElementById('yahtzeeBonus'+playeri+gamei).value;
+	
+	var lowerScore = parseInt(ThreekindScore*1+FourkindScore*1+fullhouseScore*1+smstraightScore*1+lgstraightScore*1+yahtzeeScore*1+chanceScore*1+yahtzeeBonusScore*1);
+	
+	var lowerScoreDiv = document.getElementById('totallower'+playeri+gamei);
+	lowerScoreDiv.innerHTML = lowerScore;
+	
+	return lowerScore;
+} 
+
+//function gets the total of both the upper and lower sections
+function getGrandTotal(playeri, gamei){
+	var grandtotalDiv = document.getElementById('grandtotal'+playeri+gamei);
+	var upperScoreValue = getUpperScoreAndBonus(playeri, gamei);
+	var lowerScoreValue = getLowerScore(playeri,gamei);
+
+	var grandTotal = lowerScoreValue + upperScoreValue;
+	grandtotalDiv.innerHTML = grandTotal;
+	getPlayerScore(playeri);
+	return grandTotal;
+}
+
+//function to get the running total for each player for all games
+function getPlayerScore(playeri){
+	var playerScoreDiv = document.getElementById('playerScore'+playeri);
+	var score = 0;
+	
+	for (i=0; i<totalGames; i++){
+		var thisScore = document.getElementById('grandtotal'+playeri+i).innerHTML;
+		if (thisScore != undefined && thisScore != '') {
+			score = score + parseInt(thisScore)*1;
+		}
+	}
+	
+	playerScoreDiv.innerHTML = "- " + score;
+}
+
+var rollCount = 0;
+
 function rollDice() {
 	var total = 0;
 	var dicetotal = document.getElementById('dicetotal');
@@ -18,7 +223,7 @@ function rollDice() {
 		var diceval = document.getElementById('dice' + i + 'val');
 		var dicesave = document.getElementById('dice' + i + 'h');
 		
-		if (dicesave.value !== 'save') {
+		if (dicesave.value != 'save') {
 			var num = randomNumber();
 			dice.innerHTML = getDiceImage(num, '', 'dice' + i);
 			diceval.value = num;
@@ -28,17 +233,6 @@ function rollDice() {
 	dicetotal.innerHTML = total;
 }
 
-//function testRolls(rollCount){
-//    if(rollCount<3){
-//        rollCount++;
-//        return rollCount;
-//    }
-//    else {
-//        print ("You've already rolled 3 times!");
-//        //do user input into scoresheet
-//    }
-//    
-//}
 function resetDice() {
 	
 	//make sure we're not saving any of the dice values
@@ -69,22 +263,22 @@ function toggleSave(dice) {
 function getDiceImage(num, color, dice) {
 	
 	if (num == 1){
-		return "1" + "<img src='../images/dice1.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Ace" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	} else if (num == 2) {
-		return "2"+ "<img src='../images/dice2.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Two" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	} else if (num == 3) {
-		return "3"+ "<img src='../images/dice3.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Three" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	} else if (num == 4) {
-		return "4" + "<img src='../images/dice4.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Four" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	} else if (num == 5) {
-		return "5" + "<img src='../images/dice5.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Five" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	} else if (num == 6) {
-		return "6" + "<img src='../images/dice6.png" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
+		return "<img src='images/Six" + color + ".gif' onclick='toggleSave(\"" + dice + "\")' />";
 	}
 }
 
 function randomNumber() {
-	var ran_unrounded = Math.random()*6;
-	var ran_number = Math.floor(ran_unrounded)+1;
+	var ran_unrounded=Math.random()*6;
+	var ran_number=Math.floor(ran_unrounded)+1;
 	return ran_number;
 }
